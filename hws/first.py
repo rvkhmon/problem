7cb4cc533265143bd0b633ee5746fe5bce0506d2
rvkhmon <76044622+rvkhmon@users.noreply.github.com>
2020-12-24 17:54:47 +0500

    Create first.py
    first problem 3*

---
+++ hws/first.py
@@ -0,0 +1,32 @@
+danet = int(input('Privet, my name is Mahmud matematik! Hochew podrujitsa ? vibiray 1 esli da 0 esli net '))
+if danet >= 1:
+    print('mmm, uje interestno')
+    age = int(input("A kakogo ty goda?"))
+    if age >=2003:
+        print("mojew ostavit nomer mamy XD")
+    else:
+        print("Znacit mojem prodoljit)")
+        pol = int(input('Ty devuwka ? '))
+    if pol >= 1:
+        print('Hi, moya sladkaya')
+        name = str(input("A kak k tebe obrawatsya ?) "))
+        print("i love you " + name)
+        vprs = int(input("hocew ya pomogu tebe s matematikoy ?"))
+        if vprs >= 1:
+            print("mojew napisat dva cisla)")
+            csl1 = int(input('pervoe cislo '))
+            csl2 = int(input('vtoroe cislo '))
+            print(csl1 + csl2)
+            print(csl1 - csl2)
+            print(csl1 * csl2)
+            print(csl1 / csl2)
+            nomer = int(input('mojew na posledok ostavit nomer budu ocen rad)'))
+        else:
+            print('Togda mojew ostavit nomer, budu ocen rad)')
+            
+    else:
+        print('mmm, ponyatno')
+        name = str(input("A kak k tebe obrawatsya ?) "))
+        print("ischezni otsyuda " + name)
+else:
+    print('WF poka .|.')
